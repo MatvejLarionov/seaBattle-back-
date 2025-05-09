db704ae87a6a458ece5878b966162501d536b029
Matvey <matvejlarionovit@gamil.com>
2025-05-09 18:27:53 +0300

    install cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http")
+const cors = require("cors")
 const WebSocket = require("ws")
 const express = require('express')
 const app = express()
@@ -10,6 +11,7 @@ const port = 3000
 const server = http.createServer(app)
 const wsServer = new WebSocket.Server({ server })
 
+app.use(cors({ origin: "http://localhost:5173" }))
 app.use('/users', usersRouter)
 app.use(express.static('./public'))
 app.use((req, res) => res.status(404).send("<h2>Not found</h2>"))
